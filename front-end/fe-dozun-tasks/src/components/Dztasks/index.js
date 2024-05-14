5e2cc69586306393ea9a0864fe195290eeb15416
iamdwn <truongdinhdongduong@gmail.com>
2024-05-14 17:58:53 +0700

    update html and css

---
+++ front-end/fe-dozun-tasks/src/components/Dztasks/index.js
@@ -0,0 +1,43 @@
+const Dztasks = () => {
+    return (
+        <main id="todolist">
+        <h1>
+            Dozun Tasks
+            <span>Just do it !!!!!</span>
+        </h1>
+
+        <li class="done">
+            <span class="label">123</span>
+            <div class="actions">
+                <button class="btn-picto" type="button">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <button class="btn-picto" type="button" aria-label="Delete" title="Delete">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </div>
+        </li>
+        <li>
+            <span class="label">123</span>
+            <div class="actions">
+                <button class="btn-picto" type="button">
+                    <i class="fas fa-user-edit"></i>
+                </button>
+                <button class="btn-picto" type="button" aria-label="Delete" title="Delete">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </div>
+        </li>
+        <p> </p>
+
+        <form>
+            <label htmlFor="name">Add a new task in here</label>
+            <input type="text" name="name" id="name"/>
+            <input type="text" name="id" id="name"/>
+            <button type="button">Add new</button>
+        </form>
+    </main>
+    )
+}
+
+export default Dztasks;
