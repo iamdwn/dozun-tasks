80636b6d114f14a90483af19d30da6d4c50d949f
iamdwn <truongdinhdongduong@gmail.com>
2024-05-16 21:22:14 +0700

    config cors

---
+++ back-end/be-dozun-tasks/be-dozun-tasks/Controllers/DztasksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace be_dozun_tasks.Controllers
+{
+    [Route("v1/api/dztasks")]
+    [ApiController]
+    public class DztasksController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [HttpGet("/{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost("/{id}")]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        [HttpPut("/{id}")]
+        public void Put([FromBody] string value)
+        {
+        
+        }
+
+        [HttpDelete("/{id}")]
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
+
